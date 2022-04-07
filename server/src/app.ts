d8c9ef0dbe51a619c9e1f1e7fdc8103c017c4795
AwalHossain <waqin21@gmail.com>
2022-04-07 11:45:47 +0600

    all file added

---
+++ server/src/app.ts
@@ -0,0 +1,18 @@
+console.log("hleolaf");
+
+import express from "express";
+import { createServer } from "http";
+import { Server } from "socket.io";
+
+const app = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
+  cors: {
+    origin: "http://localhost:3000",
+    credentials: true,
+  },
+});
+
+httpServer.listen(4000, () => {
+  console.log(`This app is running on 4000`);
+});
